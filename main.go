2e9790a24b510682510683786d44d5aebcd3060a
Travis Johnson <tjohnson@hungryfishmedia.com>
2013-03-22 14:28:36 -0400

    added a get function for thread saftey

--- main.go
+++ main.go
@@ -1,26 +1,37 @@
 package logler
 
 import (
+	"io"
 	"log"
 )
 
 type Logger struct {
-	Logger    log.Logger
+	Logger    *log.Logger
 	levelChan chan uint8
+	getChan   chan uint8
 	level     uint8
 }
 
 func (l *Logger) Set(level int) {
 	if l.levelChan == nil {
 		l.levelChan = make(chan uint8)
+		l.getChan = make(chan uint8)
 		go l.levelGo()
 	}
 	l.levelChan <- uint8(level)
 }
 
+func (l *Logger) get() int {
+	level := int(<-l.getChan)
+	return level
+}
+
 func (l *Logger) levelGo() {
 	for {
-		l.level <- l.levelChan
+		select {
+		case l.level = <-l.levelChan:
+		case l.getChan <- l.level:
+		}
 	}
 }
 
